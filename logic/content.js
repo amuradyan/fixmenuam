a5a8a320c749006a62b0707751d56c4be094f8d3
Andranik Muradyan <muradianator@gmail.com>
2018-03-11 04:46:20 +0400

    Hijacked window.b of menu.am to content script
    * Added a more or less generic open restaurant redirection

--- logic/content.js
+++ logic/content.js
@@ -1,5 +1,20 @@
-document.onreadystatechange = function () {
-  if (document.readyState === 'complete') {
-    window.location.replace('https://www.menu.am/?status=open')
+var basket;
+
+document.addEventListener('b', function(e) {
+  basket = e.detail
+  console.log(e.detail)
+});
+
+var s = document.createElement('script');
+s.id = 'injected'
+s.src = chrome.runtime.getURL('resources/script.js');
+
+(document.head || document.documentElement).appendChild(s);
+
+if (window.location.href.indexOf('status') < 0) {
+  if(window.location.href.indexOf('?') > 0) {
+    window.location.replace(window.location.href + '&status=open')
+  } else {
+    window.location.replace(window.location.href + '?status=open')
   }
 }
---
+++ resources/script.js
@@ -0,0 +1,5 @@
+document.dispatchEvent(new CustomEvent('b', {
+    detail : {
+        basket: window.b
+    }
+}));
